3a48463e401f0fd54386bd5b7ad4fcc2791d273e
Tiago Gomes <tiagogomes187@gmail.com>
2022-01-29 21:29:58 -0300

    Criando opracao de instanciacao

--- src/main/java/com/tiagogomes/cursomc/CursomcApplication.java
+++ src/main/java/com/tiagogomes/cursomc/CursomcApplication.java
@@ -1,13 +1,36 @@
 package com.tiagogomes.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tiagogomes.cursomc.domain.Categoria;
+import com.tiagogomes.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Administração");
+		Categoria cat4 = new Categoria(null, "Produção");
+		Categoria cat5 = new Categoria(null, "Literatura");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5));
+		
 	}
 
 }
